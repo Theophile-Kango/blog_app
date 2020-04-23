2ccd6c8781a5cf12512e0d94b2b9f5b2f25ed063
Theophile-Kango <fadhili.kango@gmail.com>
2020-04-24 01:22:40 +0200

    Added the CRUD system to the blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,13 @@ class PostsController < ApplicationController
     def contact
     end
     def index
+        @post = Post.all.order("created_at DESC")
     end
+
     def new
         @post = Post.new
     end
+
     def create
         @post = Post.new(post_params)
         if @post.save
@@ -16,12 +19,33 @@ class PostsController < ApplicationController
             render 'new'
         end
     end
+
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
     def show
         @post = Post.find(params[:id])
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
     end
 
     private
     def post_params
         params.require(:post).permit(:title, :content)
     end
+
 end
