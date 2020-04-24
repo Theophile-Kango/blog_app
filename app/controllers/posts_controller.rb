bcd55738fa6d23427091efda20a516583690237f
Theophile-Kango <fadhili.kango@gmail.com>
2020-04-24 12:28:15 +0200

    Added validation and corrected the erros

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,52 +1,50 @@
 class PostsController < ApplicationController
-    before_action :find_post, only: [:show, :update, :edit, :destroy]
-    def about
-    end
-    def contact
-    end
-    def index
-        @post = Post.all.order("created_at DESC")
-    end
+  before_action :find_post, only: %i[show update edit destroy]
+  def about; end
 
-    def new
-        @post = Post.new
-    end
+  def contact; end
 
-    def create
-        @post = Post.new(post_params)
-        if @post.save
-            redirect_to @post
-        else
-            render 'new'
-        end
-    end
+  def index
+    @post = Post.all.order('created_at DESC')
+  end
 
-    def update
-        if @post.update(post_params)
-            redirect_to @post
-        else
-            render 'edit'
-        end
-    end
+  def new
+    @post = Post.new
+  end
 
-    def edit
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
     end
+  end
 
-    def show
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
     end
+  end
 
-    def destroy
-        @post.destroy
-        redirect_to posts_path
-    end
+  def edit; end
 
-    private
-    def post_params
-        params.require(:post).permit(:title, :content)
-    end
+  def show; end
 
-    def find_post
-        @post = Post.find(params[:id])
-    end
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
 
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
-    belongs_to :post
+  belongs_to :post
+  validates :name, presence: true
+  validates :comment, length: { maximum: 140 },
+                      presence: true
 end
