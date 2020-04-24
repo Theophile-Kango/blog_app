b79d55b791d2d425e17dd4ede87a5c106078b22f
Theophile-Kango <fadhili.kango@gmail.com>
2020-04-24 02:37:56 +0200

    Added comment model and added the relation with post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    before_action :find_post, only: [:show, :update, :edit, :destroy]
     def about
     end
     def contact
@@ -21,7 +22,6 @@ class PostsController < ApplicationController
     end
 
     def update
-        @post = Post.find(params[:id])
         if @post.update(post_params)
             redirect_to @post
         else
@@ -30,15 +30,12 @@ class PostsController < ApplicationController
     end
 
     def edit
-        @post = Post.find(params[:id])
     end
 
     def show
-        @post = Post.find(params[:id])
     end
 
     def destroy
-        @post = Post.find(params[:id])
         @post.destroy
         redirect_to posts_path
     end
@@ -46,6 +43,10 @@ class PostsController < ApplicationController
     private
     def post_params
         params.require(:post).permit(:title, :content)
+    end
+
+    def find_post
+        @post = Post.find(params[:id])
     end
 
 end
