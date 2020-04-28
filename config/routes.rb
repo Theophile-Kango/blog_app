e849721822d3ab7716c2d91726719d7357385310
Theophile-Kango <fadhili.kango@gmail.com>
2020-04-28 17:20:03 +0200

    Finished the blog

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
   resources :tags
+  resources :authors
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_083823) do
+ActiveRecord::Schema.define(version: 2020_04_28_102104) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_authors_on_email", unique: true
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
